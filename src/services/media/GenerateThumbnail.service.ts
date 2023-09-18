b613e2eb1d28dacda5d698f1272c05301bee72cb
chabuuuu <87564111+chabuuuu@users.noreply.github.com>
2023-09-18 08:19:08 +0700

    Add progress bar for generate thumbnail service

--- src/services/media/GenerateThumbnail.service.ts
+++ src/services/media/GenerateThumbnail.service.ts
@@ -6,6 +6,7 @@ import { GenerateThumbnailInterface } from '../../interfaces/media.interface';
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const fs = require('fs');
+import { FfmpegProgressBar } from './FfmpegProgressBar.service';
 import 'reflect-metadata';
 @injectable()
 export class GenerateThumbnail implements GenerateThumbnailInterface {
@@ -27,12 +28,30 @@ export class GenerateThumbnail implements GenerateThumbnailInterface {
             '/storage/data/thumbnail/' +
             filename +
             '/%03d.png';
-        console.log(outputPattern);
-        const command = `ffmpeg -i ${file} -q:v 3 -f segment -segment_format mjpeg -segment_wrap ${numberOfFrames} -segment_time ${distance} -vf format=gray ${outputPattern}`;
+        // console.log(outputPattern);
+        // const command = `ffmpeg -i ${file} -q:v 3 -f segment -segment_format mjpeg -segment_wrap ${numberOfFrames} -segment_time ${distance} -vf format=gray ${outputPattern}`;
+        const argsCommand = [
+            '-i',
+            file,
+            '-q:v',
+            '3',
+            '-f',
+            'segment',
+            '-segment_format',
+            'mjpeg',
+            '-segment_wrap',
+            numberOfFrames.toString(),
+            '-segment_time',
+            distance.toString(),
+            '-vf',
+            'format=gray',
+            outputPattern,
+        ];
+        console.log('Generate thumbnail...');
+        const ffmpegProgressBar = new FfmpegProgressBar(file);
         try {
-            const { stdout, stderr } = await exec(command);
-            console.log('stdout:', stdout);
-            console.log('stderr:', stderr);
+            await ffmpegProgressBar.exec(argsCommand);
+            console.log('Done generate thumbnail');
         } catch (error) {
             console.error(error);
         }
